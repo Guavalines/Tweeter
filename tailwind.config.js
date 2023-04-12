5e715666fe9fd06659ed28430dd2ac7e121743db
guavalines <konstrictor@gmail.com>
2023-04-11 17:45:27 -0700

    installed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.{js, jsx, vue}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
   ]
 }
