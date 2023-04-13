cc2d8246a7bb6d0c888f8bde3e6beedf8b041db4
guavalines <konstrictor@gmail.com>
2023-04-12 19:13:22 -0700

    added username to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_055213) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_021110) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -79,8 +79,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_055213) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
